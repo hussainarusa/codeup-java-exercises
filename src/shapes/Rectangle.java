96ddab7da30bfc024fe23aff7f28cd1f01d7e63b
Arusa Hussain <hussainarusa18@gmail.com>
2023-08-14 15:34:26 -0500

    "committing java exercises  "

--- src/shapes/Rectangle.java
+++ src/shapes/Rectangle.java
@@ -1,29 +1,51 @@
 package shapes;
+//
+//public class Rectangle {
 
-public class Rectangle {
-    protected double length;
-    protected double width;
 
-    public Rectangle(double length, double width) {
-        this.length = length;
-        this.width = width;
-    }
+//    protected double length;
+//    protected double width;
+//
+//    public Rectangle(double length, double width) {
+//        this.length = length;
+//        this.width = width;
+//    }
+//
+//    public double getArea() {
+//        return length * width;
+//    }
+//
+//    public double getPerimeter() {
+//        return 2 * (length + width);
+//    }
+//
+//    public static void main(String[] args) {
+//        Rectangle rectangle = new Rectangle(5.0, 3.0);
+//        double area = rectangle.getArea();
+//        double perimeter = rectangle.getPerimeter();
+//
+//        System.out.println("Rectangle Area: " + area);
+//        System.out.println("Rectangle Perimeter: " + perimeter);
+//    }
+//}
 
-    public double getArea() {
-        return length * width;
-    }
 
+import static com.sun.tools.classfile.Module_attribute.RequiresEntry.length;
+
+public class Rectangle implements Measurable {
+
+
+    @Override
     public double getPerimeter() {
+        int width = 0;
         return 2 * (length + width);
     }
 
-    public static void main(String[] args) {
-        Rectangle rectangle = new Rectangle(5.0, 3.0);
-        double area = rectangle.getArea();
-        double perimeter = rectangle.getPerimeter();
-
-        System.out.println("Rectangle Area: " + area);
-        System.out.println("Rectangle Perimeter: " + perimeter);
+    @Override
+    public double getArea() {
+        int width = 0;
+        return length * width;
     }
 }
+
 
--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -1,10 +1,26 @@
 
 package shapes;
 
+//public class Square extends Rectangle {
+//    public Square(double side) {
+//        super(side, side);
+//    }
+//
+//    @Override
+//    public double getArea() {
+//        return length * length;
+//    }
+//
+//    @Override
+//    public double getPerimeter() {
+//        return 4 * length;
+//    }
+//}
+
+import static com.sun.tools.classfile.Module_attribute.RequiresEntry.length;
+
 public class Square extends Rectangle {
-    public Square(double side) {
-        super(side, side);
-    }
+
 
     @Override
     public double getArea() {
